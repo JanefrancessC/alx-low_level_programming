e67ebddd41e06dcfb445945375ad5f006e02e299
JanefrancessC <janefrancesschioma@gmail.com>
2023-06-16 08:51:02 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,7 @@
 #include <unistd.h>
 
 /**
- * Write a C program that prints exactly and that piece of art is useful" 
- * - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
+ * main - A program that prints "and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
  *
  * Return: Always 1 (Success)
  */
