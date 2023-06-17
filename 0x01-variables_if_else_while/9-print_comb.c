704fc59fb747fabeb4923a7d2bb9fe1404213827
JanefrancessC <janefrancesschioma@gmail.com>
2023-06-17 12:48:47 -0700

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main - Prints possible single digit combinations
+*
+*Return: Always 0.
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar('0' + num);
+		putchar((num != 9) ? ',' : '\n');
+		putchar((num != 9) ? ' ' : '\0');
+	}
+
+	return (0);
+}
