334a50ac34afd57ce1a9ae1c65bb53854a2d36f3
JanefrancessC <janefrancesschioma@gmail.com>
2023-06-27 05:58:48 -0700

    4-print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  *print_rev - prints string to stdout
+  *@str: pointer to str
+  *Return: void
+  */
+
+void print_rev(char *s)
+{
+	int counter;
+	int j = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		j++;
+	}
+	s--;
+	for (counter = j; counter > 0; counter--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
