56a9480a3683f41cf76304a9c1b3c73c587b6932
JanefrancessC <janefrancesschioma@gmail.com>
2023-07-24 13:12:41 -0700

    100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ *main - prints min num of coins for change
+ *@argc: the number of arguments in the CL
+ *@argv: an array of the CL args
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int cents, coin = 0;
+
+	if (argc == 1 || argc > 2)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	cents = atoi(argv[1]);
+
+	do {
+		if (cents >= 25)
+			cents -= 25;
+		if (cents >= 10)
+			cents -= 10;
+		if (cents >= 5)
+			cents -= 5;
+		if (cents >= 2)
+			cents -= 2;
+		if (cents >= 1)
+			cents -= 1;
+		coin += 1;
+	} while (cents > 0);
+
+	printf("%d\n", coin);
+	return (0);
+}
