f315be4d9987d75cff442d674edd6f9740fe2eb9
JanefrancessC <janefrancesschioma@gmail.com>
2023-09-28 13:50:03 -0700

    0-binary_to

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,18 +9,17 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int res = 0;
-	unsigned int i;
+	int i;
+	unsigned int result = 0;
 
-	for (i = 0; b[i] != '\0'; i++)
+	if (!b)
+		return (0);
+
+	for (i = 0; b[i]; i++)
 	{
-		if (b[i] == '1')
-			res = (res << 1) | 1;
-		else if (b[i] == '0')
-			res <<= 1;
-		else if (b[i] != '0' && b[i] != '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
+		result = (2 * result) + (b[i] - '0');
 	}
-
-	return (res);
+	return (result);
 }
