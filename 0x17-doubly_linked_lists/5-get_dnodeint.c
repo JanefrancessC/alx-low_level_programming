c9172bf28a6c4dce8d48fff06f25e563f5adce63
JanefrancessC <janefrancesschioma@gmail.com>
2023-11-09 14:10:02 -0800

    5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -5,7 +5,7 @@
  * @head: pointer to head of the node
  * @index: index of the node, starting from 0
  * Return: NULL if node does not exist
- * */
+ */
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
@@ -14,7 +14,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	while (head)
 	{
 		if (c == index)
-			return head;
+			return (head);
 		head = head->next;
 		c++;
 	}
